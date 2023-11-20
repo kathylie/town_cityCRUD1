2d75f5dce2eb8f5ee6c265a4305e967a586ed817
kathylie <141924739+kathylie@users.noreply.github.com>
2023-11-20 21:03:15 +0800

    Update town_delete.php

--- views/town_delete.php
+++ views/town_delete.php
@@ -1,19 +1,26 @@
 <?php
-include_once("../db.php"); // Include the Database class file
-include_once("../student.php"); // Include the Student class file
+
+include_once(__DIR__."/../db.php");
+include_once(__DIR__."/../town_city.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['id'])) {
     $id = $_GET['id']; // Retrieve the 'id' from the URL
 
-    // Instantiate the Database and Student classes
+    // Instantiate the Database and TownCity classes
     $db = new Database();
-    $town = new town_city($db);
+    $townCity = new Town_City($db);
 
-    // Call the delete method to delete the student record
-    if ($town->delete($id)) {
-        echo "Record deleted successfully.";
-    } else {
-        echo "Failed to delete the record.";
+    // Call the delete method to delete the town_city record
+    try {
+        if ($townCity->delete($id)) {
+            echo "Record deleted successfully.";
+        } else {
+            echo "Failed to delete the record.";
+        }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+        // Handle the exception as needed (log, display, etc.)
     }
 }
+
 ?>
