b5d8f578616cfe3637a207998ecb24bba3e13943
kathylie <141924739+kathylie@users.noreply.github.com>
2023-11-20 21:02:47 +0800

    Update town_add.php

--- views/town_add.php
+++ views/town_add.php
@@ -5,8 +5,8 @@ include_once("../student_details.php"); // Include the Student class file
 include_once("../town_city.php");
 //include_once("../province.php");
 
-
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $data = [    
@@ -15,30 +15,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     ];
 
     // Instantiate the Database and Student classes
-    $database = new Database();
-    $town_city = new town_city($database);
-    $town_city = $town_city->create($data);
+    // Instantiate the Database and Student classes
+$database = new Database();
+$town_cityModel = new town_city($database);
+$town_city = $town_cityModel->create($data);
+
     
-    if ($id) {
+    if ($town_city['id']) {
         // Student record successfully created
-        
+    
         // Retrieve student details from the form
-        $studentDetailsData = [
-            'id' => $id, // Use the obtained student ID
+        $town_cityDetailsData = [
+            'id' => $town_city['id'], // Use the obtained town_city ID
             'name' => $_POST['name'],
             //  town_city details fields
         ];
-
-        // Create student details linked to the student
-        $town_cityDetails = new town_cityDetails($database);
-        
+    
+        // Create town_city details linked to the town_city
+        $town_cityDetails = new $town_cityDetailsData($database);
+    
         if ($town_cityDetails->create($town_cityDetailsData)) {
             echo "Record inserted successfully.";
         } else {
             echo "Failed to insert the record.";
         }
     }
-
-    
 }
-?>
